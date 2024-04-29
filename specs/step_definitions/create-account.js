fab9ea98381377b047f549e5617281c1c1dc69a2
pudders <ericpudney@gmail.com>
2024-04-29 16:19:07 +0200

    fortfarande inte riktigt bra men det funkar någorlunda

--- specs/step_definitions/create-account.js
+++ specs/step_definitions/create-account.js
@@ -5,7 +5,7 @@ Given('that I am on the site', () => {
 });
 
 When('I click on the login button', () => {
-  cy.get('.navbar > :nth-child(2) > .d-flex > :nth-child(2) > .btn').click()
+  cy.get(':nth-child(2) > .btn').click()
 });
 
 When('I choose the register option', () => {
@@ -13,27 +13,40 @@ When('I choose the register option', () => {
 });
 
 When('I enter a valid email address and password', () => {
-  cy.get('.mb-2').type('eric@eric')
-  cy.get('[type="password"]').type('ericeric')
+  cy.get('#emailInput').type('eric@email.com')
+  cy.get('#passwordInput').type('ha5gdiwd?hbxsq12sjcabx9')
   cy.get('.ms-2').click()
 });
 
 Then('my account is created', () => {
-  if (cy.get('.alert').contains('already').should('be.visible')) {
-    cy.get('.btn-secondary').click()
-  }
-  else if (cy.get('.modal-body > p').should('be.visible')) {
-    cy.get('.modal-footer > .btn').click()
-  }
+
+/*  Jag har försökt att få testet att funkar med en if/else men det verkar inte
+går ihop med en assertion. Testet kommer att sluta i en error första gång den körs, 
+men den lyckas om man kör den igen. Testet har då kollat både att det går att skapa 
+ett nytt konto, samt att det inte går att skapa två konto med samma epostadress. */
+
+if (cy.get('.alert').contains('already').should('be.visible')) {
+  cy.get('.btn-secondary').click()
+}
+else if (cy.get('.modal-body > p').should('be.visible')) {
+  cy.get('.modal-footer > .btn').click()
+}
+else {
+  cy.get('.btn-secondary').click()
+}
 });
 
+  
+  
+
+
 Then('I can log in to my account', () => {
-  cy.get('.navbar > :nth-child(2) > .d-flex > :nth-child(2) > .btn').contains('Login').should('be.visible')
-  cy.get('.navbar > :nth-child(2) > .d-flex > :nth-child(2) > .btn').click()
-  cy.get('.mb-2').type('eric@eric')
-  cy.get('[type="password"]').type('ericeric')
+  cy.get('#loginButton').contains('Login').should('be.visible')
+  cy.get('#loginButton').click()
+  cy.get('#emailInput').type('eric@email.com')
+  cy.get('#passwordInput').type('ha5gdiwd?hbxsq12sjcabx9')
   cy.get('.ms-2').click()
   cy.get('.modal-body > p').contains('Login successful').should('be.visible')
   cy.get('.modal-footer > .btn').click()
-  cy.get('.navbar > :nth-child(2) > .d-flex > :nth-child(2) > .btn').contains('Profile').should('be.visible')
+  cy.get('#loginButton').contains('Profile').should('be.visible')
 });
